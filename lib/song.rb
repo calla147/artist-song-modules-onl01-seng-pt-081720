f7333248553358649649f9ddda6f593c8c74f4d2
noella callahan <calla147@gmail.com>
2020-09-26 13:12:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,6 @@
 require 'pry'
 
 class Song
-  # The extend keyword is responsible for defining the method as a
-    # class method vs. an instance method (include keyword)
-  extend Memorable 
-  extend Findable
-  include Paramable
-  
   attr_accessor :name
   attr_reader :artist
 
@@ -16,12 +10,27 @@ class Song
     @@songs << self
   end
 
-    def self.all
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
+
+  def self.all
     @@songs
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
   end
 
   def artist=(artist)
     @artist = artist
   end
 
- end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+end
