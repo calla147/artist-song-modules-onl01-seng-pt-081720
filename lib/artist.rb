ea129ea40633040401a8349f6193d0f8b2b1ed99
noella callahan <calla147@gmail.com>
2020-09-26 13:10:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,50 @@
-require 'pry'
+require_relative 'spec_helper.rb'
 
-class Artist
-  attr_accessor :name
-  attr_reader :songs
-
-  @@artists = []
-
-  def initialize
-    @@artists << self
-    @songs = []
+describe Song do
+  before(:each) do
+    Song.reset_all
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  let!(:song) { Song.new }
+
+  it "can initialize a song" do
+    expect(song).to be_a(Song)
   end
 
-  def self.all
-    @@artists
+  it "can have a name" do
+    song.name = "Jump Around"
+    expect(song.name).to eq("Jump Around")
   end
 
-  def self.reset_all
-    self.all.clear
+  it 'converts its name to a url friendly parameter' do
+    song.name = "Jump Around"
+    expect(song.to_param).to eq("jump-around")
   end
 
-  def self.count
-    self.all.count
+  it "has an artist" do
+    song.artist = Artist.new.tap {|a| a.name = "miley"}
+    expect(song.artist).to be_a(Artist)
+    expect(song.artist.name).to eq("miley")
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
+  describe "Class methods" do
+    it "keeps track of the songs that have been created" do
+      expect(Song.all).to include(song)
+    end
+
+    it "can count how many songs have been created" do
+      expect(Song.count).to eq(1)
+    end
+
+    it "can find a song by name" do
+      song.name = "Jump Around"
+      expect(Song.find_by_name("Jump Around")).to eq(song)
+    end
+
+    it "can reset the songs that have been created" do
+      Song.reset_all
+      expect(Song.count).to eq(0)
+    end
   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
